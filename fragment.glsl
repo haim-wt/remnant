c7eefd2375280127975264458e0e71de73f80637
haim <hvanunu@gmail.com>
2023-11-19 21:13:16 -0500

    initial pr

---
+++ fragment.glsl
@@ -0,0 +1,207 @@
+#version 410 core
+
+
+// === fragment shader
+// 
+// reference
+//  * http://www.iquilezles.org/www/articles/raymarchingdf/raymarchingdf.htm
+//  * http://www.iquilezles.org/www/articles/distfunctions/distfunctions.htm
+
+uniform float time;
+uniform vec2 resolution;
+uniform vec4 light_position;
+uniform vec3 ray_origin;
+
+const float PI = 3.14159265;
+const float HALF_PI = 0.5*PI;
+const float TWO_PI = 2.0*PI;
+const float INV_PI = 1.0/PI;
+const float INV_TWO_PI = 1.0/TWO_PI;
+const float EPSILON = 1.0e-4;
+const int MAX_DIST = 30;
+
+out vec4 color;
+in vec2 TexCoords;
+
+vec3[50] objects;
+float[50] distances;
+
+float sdSphere( vec3 p, float s )
+{
+  return length(p)-5.0;
+}
+
+float sdBox( vec3 p, vec3 b )
+{
+  vec3 q = abs(p) - b;
+  return length(max(q,0.0)) + min(max(q.x,max(q.y,q.z)),0.0);
+}
+
+float opU( float d1, float d2 )
+{
+    return min(d1,d2);
+}
+
+float opS( float d1, float d2 )
+{
+    return max(-d1,d2);
+}
+
+float opI( float d1, float d2 )
+{
+    return max(d1,d2);
+}
+
+mat4 translate_matrix(float x, float y, float z) 
+{
+  mat4 m = mat4(1.0);
+	m[3][0] = x;
+	m[3][1] = y;
+	m[3][2] = z;
+	return m;
+}
+
+mat4 scale_matrix(float x, float y, float z) 
+{
+	mat4 m = mat4(1.0);
+	m[0][0] = x;
+	m[1][1] = y;
+	m[2][2] = z;
+	return m;
+}
+
+mat4 rotate_matrix(vec3 n, float theta) 
+{
+	float c = cos(theta);
+	float s = sin(theta);
+	mat4 m = mat4(1.0);
+	m[0][0] = n.x*n.x*(1.0 - c) + c;
+	m[1][0] = n.x*n.y*(1.0 - c) + n.z*s;
+	m[2][0] = n.z*n.x*(1.0 - c) - n.y*s;
+	m[0][1] = n.x*n.y*(1.0 - c) - n.z*s;
+	m[1][1] = n.y*n.y*(1.0 - c) + c;
+	m[2][1] = n.y*n.z*(1.0 - c) + n.x*s;
+	m[0][2] = n.z*n.x*(1.0 - c) + n.y*s;
+	m[1][2] = n.y*n.z*(1.0 - c) - n.x*s;
+	m[2][2] = n.z*n.z*(1.0 - c) + c;
+	return m;
+}
+
+void getObjects() {
+	for (int i=0;i<50;i++){
+		objects[i] = vec3(-10 + 4*i, -10 + 4*i,  -20 - 4*i);
+	}
+}
+
+float compute_distances(vec3 ray) 
+{
+	float min_dist = MAX_DIST;
+	for (int obj = 0 ; obj < 50; obj++) {
+		distances[obj] = sdSphere(ray - objects[obj], 2);
+		min_dist = min(min_dist, distances[obj]);
+	}
+
+	return min_dist;
+}
+
+int compute_near(vec3 ray) 
+{
+	float max_delta = 0;
+	float delta = 0;
+	int near = 0;
+
+	for (int i = 0 ; i < 50; i++) {
+		delta = sdSphere(ray - objects[i], 2);
+		delta = distances[i] - delta;
+		if (delta > max_delta) {
+			max_delta = delta;
+			near = i;
+		}
+	}
+
+	return near;
+}
+
+vec3 estimate_normal(vec3 p) 
+{
+	// float dx = compute_distance(vec3(p.x + EPSILON, p.y, p.z)) - compute_distance(vec3(p.x - EPSILON, p.y, p.z));
+	// float dy = compute_distance(vec3(p.x, p.y + EPSILON, p.z)) - compute_distance(vec3(p.x, p.y - EPSILON, p.z));
+	// float dz = compute_distance(vec3(p.x, p.y, p.z + EPSILON)) - compute_distance(vec3(p.x, p.y, p.z - EPSILON));
+	// return normalize(vec3(dx, dy, dz));
+	return vec3(0);
+}
+
+// 'p' must be normalized
+vec2 compute_texcoord(vec3 p)
+{
+	float phi = atan(p.y, p.x);
+	float theta = acos(p.z);
+	float s = phi*INV_TWO_PI;
+	float t = theta*INV_PI;
+	return vec2(s, t);
+}
+
+vec4 shade(vec3 E, vec3 N, vec3 L, vec2 st)
+{
+	float kd = clamp(dot(N, L), 0.0, 1.0);
+
+	vec3 H = normalize(E + L);
+	float ks = pow(clamp(dot(N, H), 0.0, 1.0), 1.0);
+
+	//vec4 texel = texture2D(tex, 3.0*st);
+	vec3 color = kd + ks*vec3(0.8);
+	return vec4(color, 1.0);
+}
+
+float ray_march(vec3 ray_origin, vec3 ray_direction) {
+	
+	float min_distance = compute_distances(ray_origin);
+	if (min_distance < EPSILON) {
+		return 1;
+	}
+	vec3 ray = ray_origin + min_distance * ray_direction;
+	int near = compute_near(ray);
+
+	
+
+	float d = sdSphere(ray - objects[near], 2);
+	ray += d * ray_direction ;
+
+	d = sdSphere(ray - objects[near], 2);
+
+	if (d < EPSILON) {
+		return 1;
+	}
+	return -1;
+}
+
+void main()
+{
+	float aspect = resolution.y/resolution.x;
+	vec2 uv = 2.0 * TexCoords - 1.0;
+	uv.y = aspect*uv.y;
+
+	getObjects();
+
+	vec3 ray_direction = normalize(vec3(uv - ray_origin.xy, -1.0));
+
+	vec3 P = vec3(0.0);
+	vec3 E = vec3(0.0);
+	vec3 L = vec3(0.0);
+	vec3 N = vec3(0.0);
+	vec2 st = vec2(0.0);
+
+	float t = 0.0;
+	float distance = ray_march(ray_origin, ray_direction);	
+
+	// N = estimate_normal( P );
+	// L = normalize((light_position).xyz - P);
+	// E = normalize( P );
+	//st = compute_texcoord(E);
+
+	if (distance >= 0) {
+		color = vec4(1, 1, 1, 1.0);
+	} else {
+		color = vec4(0.05, 0.0, 0.0, 1.0);
+	}
+}
---
+++ main.go
@@ -0,0 +1,169 @@
+package main
+
+/*
+Adapted from this tutorial: http://www.learnopengl.com/#!Getting-started/Shaders
+
+Shows how to pass both position and color as inputs to a shader via a VBO
+*/
+
+import (
+	"log"
+	"runtime"
+
+	"github.com/go-gl/gl/v4.1-core/gl"
+	"github.com/go-gl/glfw/v3.3/glfw"
+)
+
+const windowWidth = 800
+const windowHeight = 600
+
+var res [2]float32
+var lp [3]float32
+var ro [3]float32
+
+func init() {
+	// GLFW event handling must be run on the main OS thread
+	runtime.LockOSThread()
+	res = [2]float32{windowWidth, windowHeight}
+	lp = [3]float32{-0.1, 1.0, 1.2}
+}
+
+func main() {
+	if err := glfw.Init(); err != nil {
+		log.Fatalln("failed to inifitialize glfw:", err)
+	}
+	defer glfw.Terminate()
+
+	glfw.WindowHint(glfw.Resizable, glfw.False)
+	glfw.WindowHint(glfw.ContextVersionMajor, 4)
+	glfw.WindowHint(glfw.ContextVersionMinor, 1)
+	glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
+	glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.True)
+	window, err := glfw.CreateWindow(windowWidth, windowHeight, "basic shaders", nil, nil)
+	if err != nil {
+		panic(err)
+	}
+	window.MakeContextCurrent()
+
+	// Initialize Glow (go function bindings)
+	if err := gl.Init(); err != nil {
+		panic(err)
+	}
+
+	window.SetKeyCallback(keyCallback)
+
+	err = programLoop(window)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+/*
+ * Creates the Vertex Array Object for a triangle.
+ */
+func createTriangleVAO(vertices []float32) uint32 {
+	var vao, vbo uint32
+	gl.GenVertexArrays(1, &vao)
+	gl.GenBuffers(1, &vbo)
+
+	gl.BindVertexArray(vao)
+
+	gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
+	gl.BufferData(gl.ARRAY_BUFFER, len(vertices)*4, gl.Ptr(vertices), gl.STATIC_DRAW)
+
+	// Position attribute
+	gl.VertexAttribPointer(0, 3, gl.FLOAT, false, 5*4, nil)
+	gl.EnableVertexAttribArray(0)
+
+	// Texture coordinate attribute
+	gl.VertexAttribPointer(1, 2, gl.FLOAT, false, 5*4, gl.PtrOffset(3*4))
+	gl.EnableVertexAttribArray(1)
+
+	return vao
+}
+
+func programLoop(window *glfw.Window) error {
+
+	// the linked shader program determines how the data will be rendered
+	vertShader, err := NewShaderFromFile("vertex.glsl", gl.VERTEX_SHADER)
+	if err != nil {
+		return err
+	}
+
+	fragShader, err := NewShaderFromFile("fragment.glsl", gl.FRAGMENT_SHADER)
+	if err != nil {
+		return err
+	}
+
+	shaderProgram, err := NewProgram(vertShader, fragShader)
+	if err != nil {
+		return err
+	}
+	defer shaderProgram.Delete()
+
+	vertices := []float32{
+		// First Triangle
+		-1.0, -1.0, 0.0, 0.0, 0.0, // Bottom Left
+		1.0, -1.0, 0.0, 1.0, 0.0, // Bottom Right
+		-1.0, 1.0, 0.0, 0.0, 1.0, // Top Left
+
+		1.0, -1.0, 0.0, 1.0, 0.0, // Bottom Right
+		1.0, 1.0, 0.0, 1.0, 1.0, // Top Right
+		-1.0, 1.0, 0.0, 0.0, 1.0, // Top Left
+	}
+
+	c := 0.0
+	VAO := createTriangleVAO(vertices)
+	for !window.ShouldClose() {
+		// poll events and call their registered callbacks
+		glfw.PollEvents()
+		t := 0.0
+		// perform rendering
+		gl.ClearColor(0.0, 0.0, 0.0, 1.0)
+		gl.Clear(gl.COLOR_BUFFER_BIT)
+
+		// draw loop
+
+		c += 0.05
+		ro = [3]float32{0, 0, -float32(c)}
+
+		// draw triangle
+		prog := shaderProgram.Use()
+		gl.UseProgram(prog)
+		gl.BindVertexArray(VAO)
+		gl.DrawArrays(gl.TRIANGLES, 0, 6) // 6 vertices for two triangles
+		gl.BindVertexArray(0)
+
+		timeLocation := gl.GetUniformLocation(prog, gl.Str("time\x00"))
+		gl.Uniform1f(timeLocation, float32(t))
+
+		resolutionLocation := gl.GetUniformLocation(prog, gl.Str("resolution\x00"))
+		gl.Uniform2fv(resolutionLocation, 1, &res[0])
+
+		light_positionLocation := gl.GetUniformLocation(prog, gl.Str("light_position\x00"))
+		gl.Uniform4fv(light_positionLocation, 1, &lp[0])
+
+		origin_position := gl.GetUniformLocation(prog, gl.Str("ray_origin\x00"))
+		gl.Uniform3fv(origin_position, 1, &ro[0])
+
+		// end of draw loop
+
+		// swap in the rendered buffer
+		window.SwapBuffers()
+
+		t++
+
+	}
+
+	return nil
+}
+
+func keyCallback(window *glfw.Window, key glfw.Key, scancode int, action glfw.Action,
+	mods glfw.ModifierKey) {
+
+	// When a user presses the escape key, we set the WindowShouldClose property to true,
+	// which closes the application
+	if key == glfw.KeyEscape && action == glfw.Press {
+		window.SetShouldClose(true)
+	}
+}
